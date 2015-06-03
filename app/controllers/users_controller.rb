4f98b49953143377b10d0ded1548946ea553dea9
tvcou <tvcou.zyx77@gmail.com>
2015-06-03 15:00:39 +0000

    "mod users_controller"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(entries_attributes: [:id, :event_id, :_destroy])
+
 end
